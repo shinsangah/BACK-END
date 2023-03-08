6d9aa406e01ce2fe27eaaa24854a389b8762f588
sang ah <kr.sangah@gmail.com>
2023-03-08 14:54:10 +0900

    230308

---
+++ 03_express/server.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+const express = require('express');
+
+const server = express();
+
+const PORT = 4000;
+
+// http://localhost:4000/api
+server.use('/api', async (req, res, next) => {
+  console.log('미들웨어 1');
+  req.reqTime = new Date();
+  next();
+});
+
+server.use((req, res, next) => {
+  console.log('미들웨어 2');
+  res.send(`요청 시간은 ${req.reqTime} 입니다!`);
+});
+
+server.use((req, res, next) => {
+  console.log('미들웨어 3');
+});
+
+server.listen(PORT, () => {
+  console.log(`익스프레스 서버는 ${PORT}번 포트에서 작동 중입니다!`);
+});
+
+// 안돼!
